05b6355817e81cb3616732c169a1441c5adf9852
Robert O'Gara <rgo594@gmail.com>
2019-09-30 14:52:41 -0400

    test

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const cinStyle = {
+  fontFamily: 'Cinzel'
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Robert O'Gara
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
