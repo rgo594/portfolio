c9cd413df7397b536c8f3b587c81a07d956690e1
Robert O'Gara <rgo594@gmail.com>
2019-10-15 14:29:38 -0400

    animated background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import ProjectCard from '../src/ProjectCard.js'
 import { Button } from 'semantic-ui-react'
 import { Card, Icon, Image } from 'semantic-ui-react'
-
+import AboutMe from '../src/AboutMe'
 
 const cinStyle = {
   fontFamily: 'Cinzel'
@@ -21,30 +21,59 @@ function App() {
 
   const scrollFunction = () => {
     window.scroll({
-    top: 900,
+    top: 750,
     behavior: 'smooth'
     })
   }
   return (
-    <div className="App">
-      <header>
-      </header>
-      <div class="top-page">
-        <div class="name">
-          <h1 class='font-a'>
-            Robert O'Gara
-          </h1>
-          <i onClick={scrollFunction} class="chevron circle down big icon "></i>
+    <div>
+      <div class="area" >
+          <ul class="circles">
+            <div class="name">
+              <h1 class='font-a'>
+                Robert O'Gara
+              </h1>
+            </div>
+            <i onClick={scrollFunction} class="icon-position chevron circle down big icon "></i>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+          </ul>
         </div>
-      </div>
-      <div>
-
-      </div>
       <div>
         <ProjectCard contester={"https://github.com/rgo594/Contester"}/>
+        <AboutMe />
       </div>
     </div>
   );
 }
+
+// <div className="App">
+//   <header>
+//   </header>
+//   <div class="top-page">
+//     <div class="name">
+//       <h1 class='font-a'>
+//         Robert O'Gara
+//       </h1>
+//       <i onClick={scrollFunction} class="chevron circle down big icon "></i>
+//
+//     </div>
+//   </div>
+//   <div>
+//
+//   </div>
+//   <div>
+//     <ProjectCard contester={"https://github.com/rgo594/Contester"}/>
+//     <AboutMe />
+//   </div>
+// </div>
 
 export default App;
